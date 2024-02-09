1d5b7e1e2af605da73ddc421f39220756b7c6528
BhavanaRamyaBanala <ramyabanala12@gmail.com>
2024-02-09 14:16:37 +0530

    First Commit

---
+++ WebAPI_EMSproject/Data/EmpDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPI_EMSproject.Models;
+
+namespace WebAPI_EMSproject.Data
+{
+    public class EmpDbContext : DbContext
+    {
+        public EmpDbContext (DbContextOptions<EmpDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPI_EMSproject.Models.EmpProfile> EmpProfile { get; set; } = default!;
+    }
+}
